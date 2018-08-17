06e0a0a3d3eb6bbcbd348686b3842295db293507
Lynn <lynn9388@gmail.com>
2018-08-18 00:40:46 +0800

    Add convert list for superscript and subscript

---
+++ conver_test.go
@@ -0,0 +1,88 @@
+/*
+ * Copyright © 2018 Lynn <lynn9388@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package superscriptsubscript
+
+import (
+	"fmt"
+	"sort"
+	"testing"
+)
+
+type runes []rune
+
+func (r runes) Len() int {
+	return len(r)
+}
+
+func (r runes) Less(i, j int) bool {
+	return r[i] < r[j]
+}
+
+func (r runes) Swap(i, j int) {
+	r[i], r[j] = r[j], r[i]
+}
+
+func TestSeperate(t *testing.T) {
+	fmt.Println("Superscripts")
+	keys := getSortedKeys(superscripts)
+	for _, k := range keys {
+		fmt.Printf("%c %c\n", k, superscripts[k])
+	}
+
+	fmt.Println("\nSubscripts")
+	keys = getSortedKeys(subscripts)
+	for _, k := range keys {
+		fmt.Printf("%c %c\n", k, subscripts[k])
+	}
+}
+
+func TestCombine(t *testing.T) {
+	set := make(map[rune]struct{})
+	for k := range superscripts {
+		set[k] = struct{}{}
+	}
+	for k := range subscripts {
+		set[k] = struct{}{}
+	}
+
+	var keys []rune
+	for k := range set {
+		keys = append(keys, k)
+	}
+	sort.Sort(runes(keys))
+
+	for _, k := range keys {
+		superscript, ok := superscripts[k]
+		if !ok {
+			superscript = ' '
+		}
+		subscript, ok := subscripts[k]
+		if !ok {
+			subscript = ' '
+		}
+		fmt.Printf("%c %c %c\n", k, superscript, subscript)
+	}
+}
+
+func getSortedKeys(m map[rune]rune) []rune {
+	var keys []rune
+	for key := range m {
+		keys = append(keys, key)
+	}
+	sort.Sort(runes(keys))
+	return keys
+}
